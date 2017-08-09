0185ffc9166bea078d394dce9ce319bbf53368c3
Dan Nguyen <dtn3423@gmail.com>
2017-08-09 12:32:42 -0400

    updated search bar, add js jQuery

--- frontend/wikipedia-viewer/main.js
+++ frontend/wikipedia-viewer/main.js
@@ -1,5 +1,17 @@
 /* JavaScript file for Wikipedia Viewer */
 
-$(document).ready(function() {
+var input;
 
+function results() {
+  $("#searchInput").bind('keypress', function(event) {
+    if (event.keyCode == 13) {
+      input = document.getElementById('searchInput').value
+      event.preventDefault();
+    };
+  });
+};
+
+
+$(document).ready(function() {
+  results();
 });
