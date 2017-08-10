4e6f6eb4665ba909c6bb08b39e782a78df62f87f
Dan Nguyen <dtn3423@gmail.com>
2017-08-10 14:23:02 -0400

    button and enter get input

--- frontend/wikipedia-viewer/main.js
+++ frontend/wikipedia-viewer/main.js
@@ -6,35 +6,39 @@ $(document).ready(function() {
   results();
 });
 
-// Store input from search bar when 'enter' is pressed
 function results() {
+  // Store input from search bar when button pressed
+  $("#hitSearch").click(function() {
+    // Animate positioning
+    $("#wikibox").animate({bottom: "15em", top: 0, left: 0, right: 0});
+    $(function() {
+      $(document).click(function() {
+        $("#wikibox").animate({bottom: "3em", top: 0, left: 0, right: 0});
+      });
+    });
+
+    // Clear results each time 'enter'
+    searchResults = [];
+    input = $("#searchInput").val();
+
+    // Get search data based on input
+    if (input) {
+      $.getJSON("https://en.wikipedia.org/w/api.php?action=opensearch&format=json&search=" + encodeURI(input) + "&callback=?", function(data) {
+        for (var i=0; i<data[1].length; i++){
+          searchResults.push(data[1][i]);
+        }
+      });
+    }
+
+    // If nothing is entered, alert user
+    else {alert("Please enter something into the search field");}
+    return false;
+  });
+
+  // Store input from search bar when 'enter' pressed
   $("#searchInput").keypress(function(e) {
     if (e.which == 13) {
-
-      // Animate positioning
-      $("#wikibox").animate({bottom: "15em", top: 0, left: 0, right: 0});
-      $(function() {
-        $(document).click(function() {
-          $("#wikibox").animate({bottom: "3em", top: 0, left: 0, right: 0});
-        });
-      });
-
-      // Clear results each time 'enter'
-      searchResults = [];
-      input = $("#searchInput").val();
-
-      // Get search data based on input
-      if (input) {
-        $.getJSON("https://en.wikipedia.org/w/api.php?action=opensearch&format=json&search=" + encodeURI(input) + "&callback=?", function(data) {
-          for (var i=0; i<data[1].length; i++){
-            searchResults.push(data[1][i]);
-          }
-        });
-      }
-
-      // If nothing is entered, alert user
-      else {alert("Please enter something into the search field");}
-
+      $("#hitSearch").click();
     return false;
     }
   });
