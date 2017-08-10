6b8e7088523b34838f76fffba7fde1eaf0037ff8
Dan Nguyen <dtn3423@gmail.com>
2017-08-09 22:59:52 -0400

    store input

--- frontend/wikipedia-viewer/main.js
+++ frontend/wikipedia-viewer/main.js
@@ -1,17 +1,24 @@
 /* JavaScript file for Wikipedia Viewer */
 
-var input;
-
-function results() {
-  $("#searchInput").bind('keypress', function(event) {
-    if (event.keyCode == 13) {
-      input = document.getElementById('searchInput').value
-      event.preventDefault();
-    };
-  });
-};
-
+var input; var searchResults = [];
 
 $(document).ready(function() {
   results();
 });
+
+// Store input from search bar when 'enter' is pressed
+function results() {
+  $('#searchInput').keypress(function(e) {
+    if (e.which == 13) {
+      input = $("#searchInput").val();
+
+      // Get search data based on input
+      $.getJSON("https://en.wikipedia.org/w/api.php?action=opensearch&format=json&search=" + encodeURI(input) + "&callback=?", function(data) {
+        for (var i=0; i<data[1].length; i++){
+          searchResults.push(data[1][i]);
+        };
+      });
+      return false;
+    };
+  })
+};
