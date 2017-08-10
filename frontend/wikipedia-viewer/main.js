ed6912e9f02961fcba0b0993160e242c0342d6a6
Dan Nguyen <dtn3423@gmail.com>
2017-08-09 23:51:04 -0400

    refined search parameters

--- frontend/wikipedia-viewer/main.js
+++ frontend/wikipedia-viewer/main.js
@@ -10,15 +10,24 @@ $(document).ready(function() {
 function results() {
   $('#searchInput').keypress(function(e) {
     if (e.which == 13) {
+      
+      // Clear results each time 'enter' or the onclear button is pressed
+      searchResults = [];
       input = $("#searchInput").val();
 
       // Get search data based on input
-      $.getJSON("https://en.wikipedia.org/w/api.php?action=opensearch&format=json&search=" + encodeURI(input) + "&callback=?", function(data) {
-        for (var i=0; i<data[1].length; i++){
-          searchResults.push(data[1][i]);
-        };
-      });
-      return false;
-    };
-  })
+      if (input) {
+        $.getJSON("https://en.wikipedia.org/w/api.php?action=opensearch&format=json&search=" + encodeURI(input) + "&callback=?", function(data) {
+          for (var i=0; i<data[1].length; i++){
+            searchResults.push(data[1][i]);
+          }
+        });
+      }
+
+      // If nothing is entered, alert user
+      else {alert("Please enter something into the search field");}
+
+    return false;
+    }
+  });
 };
