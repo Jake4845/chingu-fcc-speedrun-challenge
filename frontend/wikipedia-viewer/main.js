d643b8d8bb17dcf15e086e896cd65851c2d59d32
Dan Nguyen <dtn3423@gmail.com>
2017-08-11 13:00:04 -0400

    fixed animation reset

--- frontend/wikipedia-viewer/main.js
+++ frontend/wikipedia-viewer/main.js
@@ -1,17 +1,18 @@
 /* JavaScript file for Wikipedia Viewer */
 
 var input; var searchResults = []; var searchInfo = []; var searchLinks = [];
+var contentclone = $(".content").clone();
 
 $(document).ready(function() {
   results();
 });
 
 function results(callback) {
-  $("#fail").hide();
-  //$(".content").hide();
+  $(".content").hide();
   $("#hitSearch").click(function() {
-    // Clear results each time 'enter'
+    // Clear results each time search is made
     searchResults = []; searchInfo = []; searchLinks = [];
+    $(".contentinfo").remove();
     input = $("#searchInput").val();
     // Get search data based on input
     if (input) {
@@ -42,25 +43,25 @@ function results(callback) {
 function showResults() {
   animations();
   if (searchResults.length > 0) {
-    $(".content").append("<div class='contentinfo'><h1 class='highlight'>Title Here</h1><p>Info Here</p><a class='highlight'>Link Here<a/></div>")
+    $(".content").append("<div class='contentinfo'><h1 class='highlight'>Title Here</h1><p>Info Here</p><a class='highlight'>Link Here<a/></div>");
+    $(".content").fadeOut(1).fadeIn(1500);
     console.log(input, searchResults, searchInfo, searchLinks);
   }
   else {
     // Suggest alternative search options
-    $("#fail").append("<div class='contentinfo'><p>Sorry, there were no results matching your search.</p><p>The page <span class='highlight'><b>\"" + $("#searchInput").val() + "\"</b></span> does not exist.</p><ul><li>Check that all keywords are spelled correctly</li><li>Try using more general keywords</li><li>Try entering a shorter search if it is too long</li></ul></div>");
-    $("#fail").fadeIn(1500);
-    console.log(input, searchResults, searchInfo, searchLinks);
+    $(".content").append("<div class='contentinfo'><p>Sorry, there were no results matching your search.</p><p>The page <span class='highlight'><b>\"" + $("#searchInput").val() + "\"</b></span> does not exist.</p><ul><li>Check that all keywords are spelled correctly</li><li>Try using more general keywords</li><li>Try entering a shorter search if it is too long</li></ul></div>");
+    $(".content").fadeOut(1).fadeIn(1500);
   }
 };
 
 function animations() {
   // Animate positioning
-  $("#wikibox").animate({bottom: "21em", top: 0, left: 0, right: 0});
+  $("#wikibox").animate({bottom: "21em", top: 0, left: 0, right: 0}, 750);
   // Allow user to reset search
   $("#refresh").click(function() {
-    $("#wikibox").animate({bottom: "3em", top: 0, left: 0, right: 0});
-    input = undefined; searchResults = [];
-    $(".contentinfo").detach();
+    $("#wikibox").animate({bottom: "3em", top: 0, left: 0, right: 0}, 750);
+    input = undefined; searchResults = []; searchInfo = []; searchLinks = [];
+    $(".contentinfo").remove();
     $("#form")[0].reset();
     });
 }
