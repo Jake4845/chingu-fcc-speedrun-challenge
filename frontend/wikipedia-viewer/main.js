929cc8695709475d0e78a1dded0c988b2d71df2d
Dan Nguyen <dtn3423@gmail.com>
2017-08-11 02:40:22 -0400

    store wiki search data

--- frontend/wikipedia-viewer/main.js
+++ frontend/wikipedia-viewer/main.js
@@ -1,6 +1,6 @@
 /* JavaScript file for Wikipedia Viewer */
 
-var input; var searchResults = [];
+var input; var searchResults = []; var searchInfo = []; var searchLinks = [];
 
 $(document).ready(function() {
   results();
@@ -11,13 +11,15 @@ function results(callback) {
   //$(".content").hide();
   $("#hitSearch").click(function() {
     // Clear results each time 'enter'
-    searchResults = [];
+    searchResults = []; searchInfo = []; searchLinks = [];
     input = $("#searchInput").val();
     // Get search data based on input
     if (input) {
       $.getJSON("https://en.wikipedia.org/w/api.php?action=opensearch&format=json&search=" + encodeURI(input) + "&callback=?", function(data) {
         for (var i=0; i<data[1].length; i++){
           searchResults.push(data[1][i]);
+          searchInfo.push(data[2][i]);
+          searchLinks.push(data[3][i]);
         }
         showResults();
       });
@@ -41,16 +43,13 @@ function showResults() {
   animations();
   if (searchResults.length > 0) {
     $(".content").append("<div class='contentinfo'><h1 class='highlight'>Title Here</h1><p>Info Here</p><a class='highlight'>Link Here<a/></div>")
-    console.log(input);
-    console.log(searchResults);
-    //$("t_hold").html();
+    console.log(input, searchResults, searchInfo, searchLinks);
   }
   else {
     // Suggest alternative search options
     $("#fail").append("<div class='contentinfo'><p>Sorry, there were no results matching your search.</p><p>The page <span class='highlight'><b>\"" + $("#searchInput").val() + "\"</b></span> does not exist.</p><ul><li>Check that all keywords are spelled correctly</li><li>Try using more general keywords</li><li>Try entering a shorter search if it is too long</li></ul></div>");
     $("#fail").fadeIn(1500);
-    console.log(input);
-    console.log(searchResults);
+    console.log(input, searchResults, searchInfo, searchLinks);
   }
 };
 
@@ -61,9 +60,7 @@ function animations() {
   $("#refresh").click(function() {
     $("#wikibox").animate({bottom: "3em", top: 0, left: 0, right: 0});
     input = undefined; searchResults = [];
-    $(".nothing").detach();
+    $(".contentinfo").detach();
     $("#form")[0].reset();
     });
-  // Clear the div for 'no results'
-  $(".nothing").detach()
 }
