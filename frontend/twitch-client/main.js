44120a02c7bee252b4bcfa25fb3ab18515f3a12e
Dan Nguyen <dtn3423@gmail.com>
2017-08-18 18:01:47 -0400

    get json data for streams

--- frontend/twitch-client/main.js
+++ frontend/twitch-client/main.js
@@ -1,8 +1,25 @@
 /* JS for twitch-client app */
 
+// Store stream names
+var api = "https://wind-bow.glitch.me/twitch-api/streams/";
+var streams = ["ESL_SC2", "OgamingSC2", "cretetion", "freecodecamp", "storbeck", "habathcx", "RobotCaleb", "noobs2ninjas"]
+var fullStream = []; var sData;
+
 $(document).ready(function() {
-  // Do something here
+  fullStream = streams.map(showStreams);
+  sData = fullStream.map(streamData);
 });
 
 
-// Add function to show selected navtab for streams (all, online, offline)
+// Get URL for all streams
+function showStreams(name) {
+  return("" + api + name + "");
+};
+
+// Get data for all streams
+function streamData(data) {
+  $.getJSON("" + data + "", function(data2) {
+    console.log(data2);
+    console.log(data);
+  });
+};
