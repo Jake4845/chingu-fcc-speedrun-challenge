a99d51336755426f6fbc8e33045bc743c3e33f7d
Dan Nguyen <dtn3423@gmail.com>
2017-08-18 21:43:58 -0400

    collect stream data for online and offline

--- frontend/twitch-client/main.js
+++ frontend/twitch-client/main.js
@@ -3,11 +3,10 @@
 // Store stream names
 var api = "https://wind-bow.glitch.me/twitch-api/streams/";
 var streams = ["ESL_SC2", "OgamingSC2", "cretetion", "freecodecamp", "storbeck", "habathcx", "RobotCaleb", "noobs2ninjas"]
-var fullStream = []; var sData;
 
 $(document).ready(function() {
-  fullStream = streams.map(showStreams);
-  sData = fullStream.map(streamData);
+  // Loop through all streams for data
+  streams.map(showStreams).map(streamData);
 });
 
 
@@ -20,6 +19,15 @@ function showStreams(name) {
 function streamData(data) {
   $.getJSON("" + data + "", function(data2) {
     console.log(data2);
-    console.log(data);
+    if (data2.stream) {
+      console.log("online");
+      // Create new div for every stream online
+      $(".onbox").append("<div class='online'><div class='left'><p class='t-title'>" + data2.stream.channel.display_name + "</p><p class='t-desc'>" + data2.stream.channel.status + "</p></div><div class='right'><span class='statuson'></span></div></div>");
+    }
+    else {
+      console.log("offline");
+      // Create new div for every stream offline
+      $(".offbox").append("<div class='offline'><div class='left'><p class='t-title'>" + (data2._links.channel).slice(38) + "</p></div><div class='right'><span class='statusoff'></span></div></div>")
+    }
   });
 };
