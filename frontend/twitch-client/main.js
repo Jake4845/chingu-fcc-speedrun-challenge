385102b0810aaa473ac34f6105d5b715bb913bbd
Dan Nguyen <dtn3423@gmail.com>
2017-08-20 00:17:36 -0400

    adjusted colors

--- frontend/twitch-client/main.js
+++ frontend/twitch-client/main.js
@@ -2,7 +2,7 @@
 
 // Store stream names and URLs
 var api = "https://wind-bow.glitch.me/twitch-api/streams/";
-var streams = ["ESL_SC2", "OgamingSC2", "cretetion", "freecodecamp", "storbeck", "habathcx", "RobotCaleb", "noobs2ninjas"]
+var streams = ["freecodecamp", "ESL_SC2", "OgamingSC2", "aimbotcalvin", "Surefour", "PlayOverwatch", "harbleu", "BazzaGazza", "Loserfruit", "codeyniku", "Grimmmz"]
 
 $(document).ready(function() {
   $(".onbox").hide();
